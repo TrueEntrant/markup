3298ffb02ddc257b217ed2b1eb07b0d584845acb
Entrant <trueentrant@gmail.com>
2018-05-31 11:49:00 +0300

    first adaptive added

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 $('.slider').slick({
     dots: true
 });
+// $(".hiden_menu, .nav").hover(function(){
+//     $(".menu").toggleClass("active");
+// });
+$(".hiden_menu").click(function(){
+    $(".menu").toggleClass("active");
+});
+$(".nav").click(function(){
+    $(".menu").toggleClass("active");
+});
