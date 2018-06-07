b494f2cbae6dfd97f30558d558853440bdf913a0
Entrant <trueentrant@gmail.com>
2018-06-07 14:03:13 +0300

    finished second block with slider on it

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 $('.slider').slick({
-    dots: true
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 10000
+});
+$('.stylist').slick({
+    autoplay: true,
+    autoplaySpeed: 20000
 });
 // $(".hiden_menu, .nav").hover(function(){
 //     $(".menu").toggleClass("active");
@@ -10,3 +16,8 @@ $(".hiden_menu").click(function(){
 $(".nav").click(function(){
     $(".menu").toggleClass("active");
 });
+
+
+function catet(gip) {
+    return Math.round(Math.sqrt((Math.pow(gip, 2)) / 2));
+};
