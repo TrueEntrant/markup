77496218e9e12f146a72a05ce6a212f9fa3f459b
Entrant <trueentrant@gmail.com>
2018-06-08 17:10:14 +0300

    midle part without adaptive and bad player

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 $('.slider').slick({
     dots: true,
     autoplay: true,
-    autoplaySpeed: 10000
+    autoplaySpeed: 10000,
+    responsive: [{
+        breakpoint: 500,
+        settings: "unslick"
+      }]
 });
 $('.stylist').slick({
     autoplay: true,
-    autoplaySpeed: 20000
+    autoplaySpeed: 20000,
+    responsive: [{
+        breakpoint: 500,
+        settings: "unslick"
+      }]
+});
+$('.best_slider').slick({
+    autoplay: true,
+    autoplaySpeed: 7000,
+    responsive: [{
+        breakpoint: 500,
+        settings: "unslick"
+      }]
+});
+$('.late_slider').slick({
+    autoplay: true,
+    autoplaySpeed: 9000,
+    responsive: [{
+        breakpoint: 500,
+        settings: "unslick"
+      }]
 });
 // $(".hiden_menu, .nav").hover(function(){
 //     $(".menu").toggleClass("active");
